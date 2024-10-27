0489859c8757ca0844f3d1ef7be66851f49eb91a
fanxinhao <fanxinhao@bytedance.com>
2024-10-27 23:35:49 +0800

    feat: alter param to url

--- src/method/word2story.ts
+++ src/method/word2story.ts
@@ -13,13 +13,14 @@ type Word2StoryResponse = {
 
 async function word2story(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 	// 1. check params
-	const requestBody: Word2StoryRequest = await request.json();
-	if (!requestBody.words || !Array.isArray(requestBody.words)) {
+	const urlParams = new URL(request.url).searchParams;
+	const words = urlParams.getAll('words');
+	if (!words || !Array.isArray(words) || words.length === 0) {
 		return new Response('Invalid request: "words" field is missing or not an array', { status: 400 });
 	}
 
 	// 2. call word2story
-	const response = await WordToStory(requestBody.words, env, ctx);
+	const response = await WordToStory(words, env, ctx);
 
 	return new Response(JSON.stringify(response), {
 		headers: { 'Content-Type': 'application/json' },
