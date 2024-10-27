c0f3a9bd2480009a1bc06ff9a5ac0f6b618309f0
fanxinhao <fanxinhao@bytedance.com>
2024-10-27 23:08:35 +0800

    feat: alter url path

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,6 @@ import { word2story } from './method/word2story';
 
 const routes = new Map<string, (request: Request, env: Env, ctx: ExecutionContext) => Promise<Response>>();
 
-routes.set('GET:/word2story', word2story);
+routes.set('GET:/call/word2story', word2story);
 
 export default routes;
