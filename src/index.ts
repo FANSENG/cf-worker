d74a511ca8441190f8aeb27226e4ededef02f75e
fanxinhao <fanxinhao@bytedance.com>
2024-10-28 16:04:07 +0800

    chore: test hono

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import routes from './router';
+// import routes from './router';
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const url = new URL(request.url);
-		const path = url.pathname;
-		const method = request.method;
-		const routeKey = `${method}:${path}`;
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		const url = new URL(request.url);
+// 		const path = url.pathname;
+// 		const method = request.method;
+// 		const routeKey = `${method}:${path}`;
 
-		const handler = routes.get(routeKey);
+// 		const handler = routes.get(routeKey);
 
-		if (handler) {
-			return handler(request, env, ctx);
-		} else {
-			return new Response(`Not Found${routeKey}`, { status: 404 });
-		}
-	},
-} satisfies ExportedHandler<Env>;
+// 		if (handler) {
+// 			return handler(request, env, ctx);
+// 		} else {
+// 			return new Response(`Not Found${routeKey}`, { status: 404 });
+// 		}
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+import { Hono } from 'hono';
+const app = new Hono();
+
+app.get('/call/word2story', (c) => c.text('Hono!'));
+
+export default app;
